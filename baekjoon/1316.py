70f7d9a7c223813e235867559bb13f3a4a0028bb
sungheeyoon <torushy@gmail.com>
2021-08-02 23:44:38 +0900

    Day16

--- baekjoon/1316.py
+++ baekjoon/1316.py
@@ -1,13 +1,25 @@
-import sys
+# import sys
 
-N = int(sys.stdin.readline().strip())
-input_list=[]
-for i in range(0,N):
-    input_list.append(str(sys.stdin.readline().strip()))
+# N = int(sys.stdin.readline().strip())
+# result = 0
+# for i in range(0,N):
+#     word = sys.stdin.readline().strip()
+#     check_list=[]
+#     for j in range(0,len(word)-1):
+#         if word[j]==word[j+1]:
+#             pass
+#         elif word[j] in word[j+1:]:
+#             result-=1
+#             break
+# print(result)
 
-print(input_list)
-
-def group_word_check(word):
-    for unit in word:
-        print(unit)
-        ###need think
+n=int(input())
+cnt=0
+for _ in range(n):
+    word=input()
+    for i in range(len(word)-1):
+        if word[i]!=word[i+1]:
+            if word[i] in word[i+1:]:
+                n-=1
+                break
+print(n)
