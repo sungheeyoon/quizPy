f66b8c0902672fe5dd6bea179d1afc51cbf9bcef
sungheeyoon <torushy@gmail.com>
2021-08-16 21:38:50 +0900

    Day23

--- baekjoon/16926.py
+++ baekjoon/16926.py
@@ -1,44 +1,36 @@
-import sys
+n, m, r = map(int, input().split(' '))
+graph = [list(map(int, input().split(' '))) for _ in range(n)]
 
-N,M,R = map(int,sys.stdin.readline().strip().split())
+for _ in range(r):
+    for i in range(min(n, m) // 2):
+        s_x, s_y = i, i
+        s_value = graph[s_x][s_y]
 
-A = [[0 for i in range(M)] for j in range(N)] 
+        for j in range(i + 1, n - i):  #аб
+            s_x = j
+            prev_value = graph[s_x][s_y]
+            graph[s_x][s_y] = s_value
+            s_value = prev_value
 
-print(A)
+        for j in range(i + 1, m - i):  #го
+            s_y = j
+            prev_value = graph[s_x][s_y]
+            graph[s_x][s_y] = s_value
+            s_value = prev_value
 
-# n, m, r = map(int, input().split(' '))
-# graph = [list(map(int, input().split(' '))) for _ in range(n)]
+        for j in range(i + 1, n - i):  #©Л
+            s_x = n - j - 1
+            prev_value = graph[s_x][s_y]
+            graph[s_x][s_y] = s_value
+            s_value = prev_value
 
-# for _ in range(r):
-#     for i in range(min(n, m) // 2):
-#         s_x, s_y = i, i
-#         s_value = graph[s_x][s_y]
+        for j in range(i + 1, m - i):  #╩С
+            s_y = m - j -1
+            prev_value = graph[s_x][s_y]
+            graph[s_x][s_y] = s_value
+            s_value = prev_value
 
-#         for j in range(i + 1, n - i):  #аб
-#             s_x = j
-#             prev_value = graph[s_x][s_y]
-#             graph[s_x][s_y] = s_value
-#             s_value = prev_value
-
-#         for j in range(i + 1, m - i):  #го
-#             s_y = j
-#             prev_value = graph[s_x][s_y]
-#             graph[s_x][s_y] = s_value
-#             s_value = prev_value
-
-#         for j in range(i + 1, n - i):  #©Л
-#             s_x = n - j - 1
-#             prev_value = graph[s_x][s_y]
-#             graph[s_x][s_y] = s_value
-#             s_value = prev_value
-
-#         for j in range(i + 1, m - i):  #╩С
-#             s_y = m - j -1
-#             prev_value = graph[s_x][s_y]
-#             graph[s_x][s_y] = s_value
-#             s_value = prev_value
-
-# for i in range(n):
-#     for j in range(m):
-#         print(graph[i][j], end=' ')
-#     print()
+for i in range(n):
+    for j in range(m):
+        print(graph[i][j], end=' ')
+    print()
